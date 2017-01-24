347907faaddbe8b875b12acfec0651d7385faa05
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-01-24 19:42:42 +0100

    Update server

--- server/src/main/java/com/yncrea/framework/Application.java
+++ server/src/main/java/com/yncrea/framework/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAsync
 @EnableWebSecurity
+@EnableWebMvc
 public class Application {
     public static void main(String[] arguments) {
         SpringApplication.run(Application.class, arguments);
