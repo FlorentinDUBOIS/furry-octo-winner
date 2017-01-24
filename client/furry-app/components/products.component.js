9072a3f43a5c2502032ea60573be24041c285cbb
miton18 <rcd18@hotmail.fr>
2017-01-24 17:18:06 +0100

    move template

--- client/furry-app/components/products.component.js
+++ client/furry-app/components/products.component.js
@@ -4,7 +4,8 @@ furryApp
   bindings: { 
       products: '=' 
   },
-  template:  '<h1>{{$ctrl.user}}</h1>',
+  //template:  '<h1>{{$ctrl.user}}</h1>',
+  templateURL: 'fury-app/templates/products.html',
            
   controller: function() {
     console.log("products: ", this.products);
