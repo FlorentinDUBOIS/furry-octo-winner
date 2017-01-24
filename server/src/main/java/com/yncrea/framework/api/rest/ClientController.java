025bd53e2d7b25d0ae468f0618d5062d9c131bcc
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-01-24 10:43:27 +0100

    remove build

--- server/src/main/java/com/yncrea/framework/api/rest/ClientController.java
+++ server/src/main/java/com/yncrea/framework/api/rest/ClientController.java
@@ -4,6 +4,7 @@ import com.yncrea.framework.entities.Client;
 import com.yncrea.framework.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
