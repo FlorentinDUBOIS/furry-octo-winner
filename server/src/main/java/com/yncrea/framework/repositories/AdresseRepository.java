1fbe3f6ab135ccfc27ec81793b287752f4c2aec2
Riouallon Vincent <riouallonvincent@gmail.com>
2017-01-25 17:59:30 +0100

    api public

--- server/src/main/java/com/yncrea/framework/repositories/AdresseRepository.java
+++ server/src/main/java/com/yncrea/framework/repositories/AdresseRepository.java
@@ -1,9 +1,10 @@
 package com.yncrea.framework.repositories;
 
+import com.yncrea.framework.entities.Adresse;
 import com.yncrea.framework.entities.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AdresseRepository extends CrudRepository<Article, String> {
+public interface AdresseRepository extends CrudRepository<Adresse, String> {
 
 
 }
