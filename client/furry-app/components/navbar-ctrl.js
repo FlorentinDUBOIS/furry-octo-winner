d4eea91676ff5d191aaba7fa98124d1c99c0c79a
guy-yann <refranguy@gmail.com>
2017-01-25 11:55:47 +0100

    Add Contact component

--- client/furry-app/components/navbar-ctrl.js
+++ client/furry-app/components/navbar-ctrl.js
@@ -2,7 +2,7 @@ furryApp.controller("navbarCtrl",function($scope,$state,$User){
     $scope.account="yolo";
     $scope.accountLink=function(){
         if ($User.isLoggedIn())
-            $state.go("information")
+            $state.go("myAccount")
         else $state.go("loginForm")
     };
 });
