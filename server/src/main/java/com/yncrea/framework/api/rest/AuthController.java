26ba3f25bae475f2a8b75dbda12e9516e2151150
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-01-25 15:29:22 +0100

    Improve authentication

--- server/src/main/java/com/yncrea/framework/api/rest/AuthController.java
+++ server/src/main/java/com/yncrea/framework/api/rest/AuthController.java
@@ -1,13 +1,17 @@
 package com.yncrea.framework.api.rest;
 
+import com.yncrea.framework.api.rest.exceptions.AuthenticationException;
 import com.yncrea.framework.api.rest.exceptions.ClientNotFoundException;
+import com.yncrea.framework.api.rest.pojos.ClientCredentials;
 import com.yncrea.framework.entities.Client;
 import com.yncrea.framework.security.jwt.services.AuthenticationService;
 import com.yncrea.framework.services.ClientService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -23,13 +27,17 @@ public class AuthController {
     private ClientService clientService;
 
     @PostMapping("/api/auth/{Id}")
-    public HashMap<String, String> create(@PathVariable(name = "Id") String Id) throws ClientNotFoundException {
+    public HashMap<String, String> create(@PathVariable(name = "Id") String Id, @RequestBody ClientCredentials clientCredentials) throws ClientNotFoundException, AuthenticationException {
         if (!clientService.exists(Id)) {
             throw new ClientNotFoundException("Unknown client");
         }
 
         HashMap<String, String> object = new HashMap<String, String>();
         Client client = clientService.findOne(Id);
+
+        if (!BCrypt.checkpw(clientCredentials.getPassword(), client.getHash())) {
+            throw new AuthenticationException("Password is invalid.");
+        }
 
         object.put("token", authenticationService.genToken(client));
 
