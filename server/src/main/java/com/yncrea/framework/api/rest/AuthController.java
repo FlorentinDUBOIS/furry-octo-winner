e0f33b86c621403402f76ff6e082e33b25d96be0
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-01-25 17:21:27 +0100

    Fix cross oigin

--- server/src/main/java/com/yncrea/framework/api/rest/AuthController.java
+++ server/src/main/java/com/yncrea/framework/api/rest/AuthController.java
@@ -8,12 +8,14 @@ import com.yncrea.framework.security.jwt.services.AuthenticationService;
 import com.yncrea.framework.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@CrossOrigin
 @RestController
 public class AuthController {
 
