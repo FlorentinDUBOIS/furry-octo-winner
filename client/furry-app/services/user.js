f2c585219da55bc03e2295b23c4f405c3370d4a3
miton18 <rcd18@hotmail.fr>
2017-01-25 12:02:50 +0100

    doc on user service

--- client/furry-app/services/user.js
+++ client/furry-app/services/user.js
@@ -1,9 +1,17 @@
 furryApp.factory('$User', ['$resource', function($http) {
   
   return {
+
+      /**
+       * Send user and password to api
+       * store JWT if success
+       * @param {string} username Email of user
+       * @param {string} password Password of user
+       * @return {Promise} 
+       */
       login: (username, password) => {
         return new Promise((resolve, reject) => {
-          return $http.post('/api/users', {
+          return $http.post('/api/user', {
               username,
               password
           })
@@ -18,6 +26,10 @@ furryApp.factory('$User', ['$resource', function($http) {
         });
       },
 
+      /**
+       * Check if current user is logged in
+       * @return {boolean} user is logged?
+       */
       isLoggedIn: () => {
         let test = localStorage.getItem('logged');
         if (!test) {
@@ -27,22 +39,29 @@ furryApp.factory('$User', ['$resource', function($http) {
         }
       },
 
+      /**
+       * Remove stored token
+       */
       logout: () => {
         if (isLoggedIn()) {
           localStorage.removeItem('logged');
         }
       },
 
+      /**
+       * Register a new user
+       * @param {Object} user Object with all user model properties
+       * @return {Promise} resolved when user is created
+       */
       register: (user) => {
-        return $http.post('/api/users', {
-            username,
-            password
-        })
-        .then((res) => {
-          localStorage.setItem('logged', true);  
-        })
-        .catch((err) => {
-          console.error('Something gone wrong when try to log in', err);
+        return new Promise((resolve, reject) => {
+          return $http.post('/api/user', {user})
+          .then((res) => {
+            resolve();
+          })
+          .catch((err) => {
+            console.error('Something gone wrong when try to register in', err);
+          });
         });
       }
   }
