43a5b61fa06363cb8610735f582d008abb1645b5
miton18 <rcd18@hotmail.fr>
2017-03-04 19:51:54 +0100

    support static files

--- server/src/main/java/com/yncrea/framework/Application.java
+++ server/src/main/java/com/yncrea/framework/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableWebMvc
-public class Application {
+public class Application extends WebMvcAutoConfiguration {
+
     public static void main(String[] arguments) {
         SpringApplication.run(Application.class, arguments);
     }
