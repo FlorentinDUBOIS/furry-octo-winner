b3d323791de21a074aaf1f2d3685b87c38b44748
Riouallon Vincent <riouallonvincent@gmail.com>
2017-03-20 19:31:12 +0100

    Toolbar translation and construction

--- server/src/main/resources/public/furry-app/components/lang.controller.js
+++ server/src/main/resources/public/furry-app/components/lang.controller.js
@@ -1,22 +1,23 @@
 furryApp.controller("langCtrl", function ($scope, $translate, $Prices) {
-        $scope.changeLanguage = function (key) {
-            $translate.use(key);
-            console.log('Change Language',key);
-        };
+    $scope.changeLanguage = function (key) {
+        $translate.use(key);
+        console.log('Change Language', key);
+    };
 
-        $Prices.availableCurrencies()
-            .then((currencies) => {
+    $Prices.availableCurrencies()
+        .then((currencies) => {
             $scope.currencies = currencies;
-        console.log($Prices.getUserCurrency())
-        $scope.userCurrency = $Prices.getUserCurrency();
+            console.log($Prices.getUserCurrency())
+            $scope.userCurrency = $Prices.getUserCurrency();
 
-        $scope.updateCurrency = () => {
-            $Prices.setUserCurrency($scope.userCurrency);
-        }
+            $scope.updateCurrency = () => {
+                $Prices.setUserCurrency($scope.userCurrency);
+            }
 
-        $scope.$apply();
-    });
-    }
+            $scope.$apply();
+        });
 
+    $scope.languages = $translate.getAvailableLanguageKeys();
+}
 
 );
