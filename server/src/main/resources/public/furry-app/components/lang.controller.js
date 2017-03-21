8c3a82c02f30f46ce2b1a99ee3e100ac98f6d8cd
Riouallon Vincent <riouallonvincent@gmail.com>
2017-03-21 11:08:06 +0100

    languages controller modification

--- server/src/main/resources/public/furry-app/components/lang.controller.js
+++ server/src/main/resources/public/furry-app/components/lang.controller.js
@@ -1,21 +1,8 @@
-furryApp.controller("langCtrl", function ($scope, $translate, $Prices) {
+furryApp.controller("langCtrl", function ($scope, $translate) {
     $scope.changeLanguage = function (key) {
         $translate.use(key);
         console.log('Change Language', key);
     };
-
-    $Prices.availableCurrencies()
-        .then((currencies) => {
-            $scope.currencies = currencies;
-            console.log($Prices.getUserCurrency())
-            $scope.userCurrency = $Prices.getUserCurrency();
-
-            $scope.updateCurrency = () => {
-                $Prices.setUserCurrency($scope.userCurrency);
-            }
-
-            $scope.$apply();
-        });
 
     $scope.languages = $translate.getAvailableLanguageKeys();
 }
