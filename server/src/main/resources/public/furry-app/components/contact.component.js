305a8db8f42015c15fbb9c1e0fcb64acae340b91
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-03-22 11:32:17 +0100

    feat(contact): Update contact view

--- server/src/main/resources/public/furry-app/components/contact.component.js
+++ server/src/main/resources/public/furry-app/components/contact.component.js
@@ -2,6 +2,31 @@
   angular
     .module('furryApp')
     .component('contactComponent', {
-      templateUrl: 'furry-app/templates/contact.html'
+      templateUrl: 'furry-app/templates/contact.html',
+      controller: contactComponent
     })
+
+    function contactComponent($scope, $mdToast, $translate, $log) {
+      $scope.$contact = {}
+      $scope.send = function($event) {
+        if (!$event.defaultPrevented) {
+          $event.preventDefault()
+        }
+
+        $log.info('Sent message', $scope.$contact)
+        $scope.$contact = {}
+
+        $translate('contact.sent').then(function(sent) {
+          $mdToast.show(
+            $mdToast
+              .simple()
+              .textContent(sent)
+          )
+        })
+
+        return false
+      }
+    }
+
+    contactComponent.$inject = ['$scope', '$mdToast', '$translate', '$log']
 } ())
